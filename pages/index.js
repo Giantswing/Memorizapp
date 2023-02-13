adae4ab4e4e4705aa2252056a351f7df19da1885
Alejandro <alejandropino88@gmail.com>
2023-02-13 19:58:23 +0100

    before installing draft

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import Header from "/components/AppHeader";
+import AppHeader from "/components/appHeader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +13,10 @@ function Home() {
         <title>Memorizapp</title>
       </Head>
 
-      <Header />
+      <AppHeader />
 
       <main className="o-container">
-        <h1>Memorizapp5</h1>
+        <h1>Memorizapp</h1>
         <p>Memorizapp3</p>
       </main>
     </div>
